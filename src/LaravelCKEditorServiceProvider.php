777d3e08919447516c20b4c875a4453a55e5b57d
Alex Shinkevich <alex.shinkevich@gmail.com>
2015-01-21 16:00:36 +0300

    Initial commit

---
+++ src/LaravelCKEditorServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace alexshin\LaravelCKEditor;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelCKEditorServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['ckeditor'] = $this->app->share(function ($app) {
+                return new Foo;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['command.ckeditor.generate', 'command.ckeditor.models'];
+    }
+    
+    
+}
+
+class Foo{}
